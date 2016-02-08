fbb844f32e199b7985521704c02ceda57d7b81a9
Artur Pañach <arturictus@gmail.com>
2016-02-08 00:50:16 -0200

    added basic behavior for contextuable class

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in contextuable.gemspec
 gemspec
+gem 'pry'
--- lib/contextuable.rb
+++ lib/contextuable.rb
@@ -3,3 +3,4 @@ require "contextuable/version"
 module Contextuable
   # Your code goes here...
 end
+require 'contextuable/base'
